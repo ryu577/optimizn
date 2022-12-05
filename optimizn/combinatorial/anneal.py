934e733336b60da8aa83fad7d62cc84d8b583f7c
Rohit Pandey <rohitpandey@Rohits-MacBook-Air.local>
2022-12-05 00:27:30 -0800

    Added simulated annealing test case for min path cover problem.

--- optimizn/combinatorial/anneal.py
+++ optimizn/combinatorial/anneal.py
@@ -19,20 +19,24 @@ class OptProblem():
 	def __init__(self):
 		''' Initialize the problem '''
 		self.candidate = self.get_candidate()
-		self.current_cost = self.cost(candidate)
+		self.current_cost = self.cost(self.candidate)
 		self.best_cost = self.current_cost
-		self.best_candidate = make_copy(candidate)
+		self.best_candidate = make_copy(self.candidate)
 
 	def anneal(self, n_iter=100000):
 		for i in range(n_iter):
+			if i % 10000 == 0:
+				print("Iteration: " + str(i) + \
+					" Current best solution: " + str(self.best_cost))
 			eps = 0.3 * e**(-i/n_iter)
-			new_candidate = make_copy(self.candidate)
-			new_cost = self.cost(self.new_candidate)
+			self.new_candidate = self.next_candidate()
+			self.new_cost = self.cost(self.new_candidate)
 			if self.new_cost < self.best_cost:
-				self.update_best(new_candidate, new_cost)
+				self.update_best(self.new_candidate, self.new_cost)
 				print("Best cost updated to:" + str(self.new_cost))
 			if self.new_cost < self.current_cost or eps < uniform():
-				self.update_candidate(new_candidate, new_cost)
+				self.update_candidate(self.new_candidate, 
+									  self.new_cost)
 
 	def update_candidate(self, candidate, cost):
 		self.candidate = make_copy(candidate)
