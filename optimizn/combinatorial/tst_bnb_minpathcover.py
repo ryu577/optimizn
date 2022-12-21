ad0eea7f0ac19f7cafe55928de1f0d640b3cc040
Akshay Sathiya <asathiya@microsoft.com>
2022-12-20 21:02:07 -0800

    branch and bound for the min path cover problem

---
+++ optimizn/combinatorial/tst_bnb_minpathcover.py
@@ -0,0 +1,91 @@
+import numpy as np
+from branch_and_bound import BnBProblem
+
+
+class MinPathCoverProblem(BnBProblem):
+
+    def __init__(self, edges1, edges2):
+        self.edges1 = edges1
+        self.edges2 = edges2
+        self.vertices = set(edges1.flatten()).union(set(edges2.flatten()))
+        self._get_all_paths()
+        print(self.all_paths)
+        super().__init__(init_sol=(np.ones(len(self.all_paths)), -1))
+
+    def _get_all_paths(self):
+        self.all_paths = []
+        for u1, u2 in self.edges1:
+            for v1, v2 in self.edges2:
+                if u2 == v1:
+                    self.all_paths.append([u1, v1, v2])
+        self.all_paths = np.array(self.all_paths)
+
+    def _pick_rem_paths(self, sol):
+        # determine which vertices can still be covered
+        choices = sol[0]
+        last_idx = sol[1] + 1
+        covered = set()
+        for i in range(0, last_idx):
+            if choices[i] == 1:
+                covered = covered.union(set(self.all_paths[i]))
+        to_cover = set(self.all_paths[last_idx:].flatten()).difference(covered)
+
+        # select paths that cover the most of the remaining vertices
+        new_path_idxs = []
+        new_covered = set()
+        while len(to_cover.difference(new_covered)) != 0:
+            path_idx = None
+            path_covered = set()
+            for i in range(last_idx, len(choices)):
+                new_verts = set(self.all_paths[i]).intersection(
+                    to_cover.difference(new_covered))
+                if len(new_verts) > len(path_covered):
+                    path_idx = i
+                    path_covered = new_verts
+            new_path_idxs.append(path_idx)
+            new_covered = new_covered.union(path_covered)
+        return new_path_idxs
+
+    def lbound(self, sol):
+        # sum of existing paths and (number of vertices left to cover / 3) 
+        choices = sol[0]
+        last_idx = sol[1] + 1
+        covered = set()
+        for i in range(0, last_idx):
+            if choices[i] == 1:
+                covered = covered.union(set(self.all_paths[i]))
+        return sum(choices[0: sol[1] + 1]) + len(self.vertices.difference(
+            covered)) / 3
+
+    def cost(self, sol):
+        return sum(sol[0])
+
+    def branch(self, sol):
+        new_sols = []
+        if sol[1] + 1 < len(sol[0]):
+            for val in [0, 1]:
+                new_sol = np.zeros(len(sol[0]))
+                new_sol[0: sol[1] + 1] = sol[0][0: sol[1] + 1]
+                new_sol[sol[1] + 1] = val
+                for idx in self._pick_rem_paths((new_sol, sol[1] + 1)):
+                    new_sol[idx] = 1
+                new_sols.append((new_sol, sol[1] + 1))
+        return new_sols
+
+    def is_sol(self, sol):
+        covered = set()
+        for i in range(len(sol[0])):
+            if sol[0][i] == 1:
+                covered = covered.union(set(self.all_paths[i]))
+        return covered == self.vertices
+
+
+def test_bnb_minpathcover():
+    edges1 = np.array([[1, 4], [2, 4], [2, 5], [3, 5]])
+    edges2 = np.array([[4, 6], [4, 7], [5, 8]])
+    scr, sol = MinPathCoverProblem(edges1, edges2).solve()
+    print(scr, sol)
+
+
+if __name__ == '__main__':
+    test_bnb_minpathcover()
